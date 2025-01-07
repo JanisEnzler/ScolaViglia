52c91925db53bf22b6f408eb68fca587dc5bb1f0
Janis Joel <52508299+JanisEnzler@users.noreply.github.com>
2025-01-07 20:59:54 +0100

    Added House and Car

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
- const title = document.querySelector('.title');
- const layer1 = document.querySelector('.layer1');
+const title = document.querySelector('.title');
+const layer1 = document.querySelector('.layer1');
+const car = document.querySelector('.car');
+const carY = car.getBoundingClientRect().bottom;
 
- document.addEventListener('scroll', function() {
-    let value = window.scrollY;
+document.addEventListener('scroll', function () {
+   let value = window.scrollY;
 
-    title.style.marginTop = value * 0.8 + 'px';
+   // Y position of the car relative to the top of the page
+   title.style.marginTop = value * 0.8 + 'px';
 
-    layer1.style.marginBottom = -value * 0.5 + 'px';
- });
+   layer1.style.marginBottom = -value * 0.5 + 'px';
+   car.style.marginLeft = -(value - carY) * 0.5 + 'px';
+   car.style.marginBottom = -(value - carY) * 0.1 + 'px';
+});
