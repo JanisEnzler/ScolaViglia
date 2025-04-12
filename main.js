6b92165901e91d14b9060514c8391c29e95b900f
Janis Joel <52508299+JanisEnzler@users.noreply.github.com>
2025-04-12 23:06:19 +0200

    Minor fixes

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const layer1 = document.querySelector('.layer1');
 
 function parallax() {
     const scroll = window.scrollY;
-    title.style.transform = `translateY(calc(${scroll * 0.7}px - 20%))`;
+    title.style.transform = `translateY(calc(${scroll * 0.7}px - 30%))`;
     layer1.style.transform = `translateY(${scroll * 0.5}px)`;
 }
 
